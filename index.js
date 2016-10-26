eb2adef7add77d247a115f5b99ee979befb2a33a
YourtionGuo <yourtion@gmail.com>
2016-10-26 17:30:36 +0800

    eslint

--- index.js
+++ index.js
@@ -10,30 +10,31 @@ const tasks = config.tasks;
 const port = config.port || 8300;
 
 function register(task) {
-  debug("Add Task: %j", task);
+  debug('Add Task: %j', task);
 
-  app.post('/'+task.name, function (req, res) {
+  app.post('/' + task.name, function (req, res) {
 
-    console.log(`Run :${task.name} - ${task.command} - ${task.token}`);
+    console.log(`Run :${ task.name } - ${ task.command } - ${ task.token }`);
 
     if(req.params.token && req.params.token !== task.token) {
-      debug.(`Token error: ${req.params.token} !== ${task.token}`);
+      debug(`Token error: ${ req.params.token } !== ${ task.token }`);
       return res.end('Token Error!');
     }
 
-    exec(task.command,function(err, stdout, stderr){
-      console.log(`stderr: ${stderr}`);
+    exec(task.command, function (err, stdout, stderr){
+      if(err) console.error(err);
+      console.log(`stderr: ${ stderr }`);
       res.end('Done!');
     });
 
   });
 }
 
-for (let task of tasks) {
+for (const task of tasks) {
   register(task);
 }
 
-const server = app.listen(8300, function () {
+const server = app.listen(port, function () {
   const host = server.address().address;
   const port = server.address().port;
   console.log('UpdateService listening at http://%s:%s', host, port);
