17fe68ad1822114fa917229bc3f9f69aaa12cf99
YourtionGuo <yourtion@gmail.com>
2017-09-08 13:12:46 +0800

    add task branch support

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const tasks = config.tasks;
 
 const port = config.port || 8300;
 
+function execCommand(command, cb) {
+  exec(command, function (err, stdout, stderr){
+    if(err) cb(err);
+    // console.log(`stderr: ${ stderr }`);
+    cb(null);
+  });
+}
+
 http.createServer((req, res) => {
   const request = url.parse(req.url, true);
   const taskName = request.pathname.slice(1);
@@ -20,19 +28,49 @@ http.createServer((req, res) => {
     res.writeHead(404);
     return res.end();
   }
+
   const task = tasks[taskName];
   if(!task) return res.end('Task Error!');
   if(task.token) {
     if(!request.query.token || request.query.token !== task.token) {
-      console.log(`Token error: ${ request.query.token } !== ${ task.token }`);
+      // console.log(`Token error: ${ request.query.token } !== ${ task.token }`);
       return res.end('Token Error!');
     }
   }
   if(!task.command) return res.end('Task Error!');
-  exec(task.command, function (err, stdout, stderr){
-    if(err) console.error(err);
-    // console.log(`stderr: ${ stderr }`);
-    res.end('Done!');
-  });
 
+  if(req.method === 'POST' && task.type && task.branch) {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      try {
+        const json = JSON.parse(bodyString);
+        let run = false;
+        switch (task.type) {
+        case 'gitlab':
+          run = json.ref.indexOf(task.branch) > -1;
+          break;
+        
+        default:
+          break;
+        }
+
+        if (run) {
+          execCommand(task.command, (err) => {
+            res.end(err || 'Done!');
+          });
+        } else {
+          res.end('OK!');
+        }
+      } catch (error) {
+        res.end(error);
+      }
+    });
+  } else {
+    execCommand(task.command, (err) => {
+      res.end(err || 'Done!');
+    });
+  }
 }).listen(port);
